de5b30da6fed1a2185feb43138f431d19e3f6c63
Henri Verroken <henriverroken@gmail.com>
2014-10-14 21:57:03 +0200

    Use and print out the screen being streamed

--- vncserver/vncserver/main.c
+++ vncserver/vncserver/main.c
@@ -15,8 +15,45 @@
 
 int main(int argc, const char **argv) {
     Streamer streamer;
+    uint32_t displaysCount;
+    CGDirectDisplayID *displayIDs;
 
-    if(!StreamerInit(&streamer, CGMainDisplayID())) {
+    if(CGGetActiveDisplayList(0, NULL, &displaysCount) != kCGErrorSuccess) {
+        fprintf(stderr, "error: CGGetActiveDisplayList: can't get count\n");
+        exit(1);
+    }
+
+    displayIDs = malloc(sizeof(*displayIDs) * displaysCount);
+    if(CGGetActiveDisplayList(displaysCount, displayIDs, &displaysCount)) {
+        fprintf(stderr, "error: CGGetActiveDisplayList: can't get IDs\n");
+        exit(1);
+    }
+
+    for(int i = 0; i < displaysCount; i++) {
+        printf("id: %d\n", (int)displayIDs[i]);
+
+        CFDictionaryRef info, localizedNames;
+        io_service_t ioPort;
+
+        ioPort = CGDisplayIOServicePort(displayIDs[i]);
+        info = IODisplayCreateInfoDictionary(ioPort,
+                                             kIODisplayOnlyPreferredName);
+        localizedNames = CFDictionaryGetValue(info, CFSTR(kDisplayProductName));
+        if(CFDictionaryGetCount(localizedNames) > 0) {
+            CFStringRef name;
+
+            CFDictionaryGetKeysAndValues(localizedNames, NULL, &name);
+            printf("%d: %s\n", (int)displayIDs[i],
+                   CFStringGetCStringPtr(name, kCFStringEncodingUTF8));
+
+        } else {
+            printf("%d: unknown\n", (int)displayIDs[i]);
+        }
+
+    }
+
+    printf("Using: %d\n", (int)displayIDs[displaysCount - 1]);
+    if(!StreamerInit(&streamer, displayIDs[displaysCount - 1])) {
         fprintf(stderr, "error: StreamerInit: %s\n",
                 StreamerCopyLastError(&streamer));
         exit(1);
@@ -29,4 +66,6 @@ int main(int argc, const char **argv) {
     }
 
     CFRunLoopRun();
+
+    return 0;
 }
