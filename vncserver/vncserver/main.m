e495a98d9c44e047d177b63055d4aa4f30ebe51f
Henri Verroken <henriverroken@gmail.com>
2014-10-17 01:26:44 +0200

    Choose display id on startup

--- vncserver/vncserver/main.m
+++ vncserver/vncserver/main.m
@@ -8,18 +8,76 @@
 
 
 #import <Foundation/Foundation.h>
+#import <ApplicationServices/ApplicationServices.h>
 
 #import "Streamer.h"
+
+CGDirectDisplayID ChooseDisplayID(void);
 
 int main(int argc, const char **argv) {
     @autoreleasepool {
         Streamer *streamer;
 
-        streamer = [[Streamer alloc] initWithDisplayID:kCGDirectMainDisplay];
+        streamer = [[Streamer alloc] initWithDisplayID:ChooseDisplayID()];
 
         [streamer startStreaming:NULL];
 
         [[NSRunLoop mainRunLoop] run];
     }
     return 0;
+}
+
+CGDirectDisplayID ChooseDisplayID(void) {
+    CGDirectDisplayID retVal;
+
+    while(1) {
+        CGDirectDisplayID *displayIDs;
+        uint32_t count, i;
+        int choice;
+
+        CGGetActiveDisplayList(0, NULL, &count);
+
+        displayIDs = malloc(sizeof(*displayIDs) * count);
+        CGGetActiveDisplayList(count, displayIDs, &count);
+
+        printf("Connected displays:\n");
+        for(i = 0; i < count; i++) {
+            CFDictionaryRef cinfo;
+            io_service_t port;
+            NSString *name = nil;
+
+            port = CGDisplayIOServicePort(displayIDs[i]);
+
+            cinfo = IODisplayCreateInfoDictionary(port, kIODisplayOnlyPreferredName);
+            if(cinfo) {
+                CFDictionaryRef cnames;
+
+                cnames = CFDictionaryGetValue(cinfo,
+                                              CFSTR(kDisplayProductName));
+                if(cnames) {
+                    const CFStringRef cname = NULL;
+                    CFDictionaryGetKeysAndValues(cnames, NULL,
+                                                 (const void **)&cname);
+                    name = [NSString stringWithString:
+                            (__bridge NSString *)cname];
+                }
+                CFRelease(cinfo);
+            }
+
+            printf("\t%i - %s (%d)\n", i, [name  UTF8String],
+                   (int)displayIDs[i]);
+        }
+
+        printf("Choose a display: ");
+        if(scanf("%d", &choice) == 1) {
+            if(choice < (int)count) {
+                retVal = displayIDs[choice];
+                free(displayIDs);
+                break;
+            }
+        }
+
+        free(displayIDs);
+    }
+    return retVal;
 }
