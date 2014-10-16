49ce3d25460c35805cbf533886093ca3544f5e0f
Henri Verroken <henriverroken@gmail.com>
2014-10-16 12:59:47 +0200

    Convert project obj-c and clean out

---
+++ vncserver/vncserver/main.m
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  vncserver
+//
+//  Created by Henri Verroken on 14/10/14.
+//  Copyright (c) 2014 hverr. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char **argv) {
+    @autoreleasepool {
+        NSLog(@"Hello World");
+    }
+    return 0;
+}
